3b5d9ad0142d26cc979b53dad0f37b384c71362e
konda659599 <126367666+konda659599@users.noreply.github.com>
2023-08-18 16:16:25 +0530

    Update Main.java

--- mymavenproject/src/main/java/Main.java
+++ mymavenproject/src/main/java/Main.java
@@ -3,7 +3,7 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int first = 35;
+		int first = 45;
 	    int second = 70;
 
 	    // add two numbers
